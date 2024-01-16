09ef8adb0fc0c75abfdb7d70a0787967cc96954b
Emerson Barbosa Tenorio <emersontenorio1@gmail.com>
2024-01-15 22:57:38 -0300

    Adicionando menu

--- go-alura/src/hello/helllo.go
+++ go-alura/src/hello/helllo.go
@@ -14,4 +14,10 @@ func main() {
 	var versao float32 = 1.1
 	fmt.Println("Olá senhor, ", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do Programa")
+	var comando int
+	fmt.Scanf("%d", &comando)
 }
